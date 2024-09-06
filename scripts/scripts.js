36205391fb3b742a50f8abf67df33ed06833021e
Moises Fausto <moises_system@yahoo.com.br>
2024-09-06 14:51:48 -0300

    criação de script para dinamismo em datas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
 const date = new Date;
+const currentYear = date.getFullYear()
 
-const exp = date.getFullYear() - 2018;
-
+// Bloco Home
+const exp =  currentYear - 2018;
 document.getElementById('exp').innerText = `${exp} anos`
+
+// Bloco Footer
+if (currentYear == 2024)
+  document.getElementById('current-year').innerText = currentYear
+else
+document.getElementById('current-year').innerText = `2024 - ${currentYear}`
